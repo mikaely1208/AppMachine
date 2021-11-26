90e85f4e259ecb98a5faab96b2e209d2fbd7a76f
Olivier Georgeon <olivier.georgeon@gmail.com>
2021-11-26 14:05:49 +0100

    OsoyooCarEnacter.py bug fix

--- OsoyooCarEnacter.py
+++ OsoyooCarEnacter.py
@@ -1,28 +1,44 @@
 # Olivier Georgeon, 2021.
 # This code is used to teach Developmental AI.
-# It requires:
-#   A robot Osoyoo Car https://osoyoo.com/2019/11/08/omni-direction-mecanum-wheel-robotic-kit-v1/
-#   Project https://github.com/OlivierGeorgeon/osoyoo
-#   Python libraries pyglet and keyboard
+# Requires:
+#   - A robot Osoyoo Car https://osoyoo.com/2019/11/08/omni-direction-mecanum-wheel-robotic-kit-v1/
+#   - Project https://github.com/OlivierGeorgeon/osoyoo
+#       (First tested with commit 3128c2c November 26th 2021)
+#   - Python libraries pyglet and keyboard
 from EgoMemoryWindow import EgoMemoryWindow
 import pyglet
 
+
 class OsoyooCarEnacter:
     def __init__(self):
-        self.em_window = EgoMemoryWindow()
-        pyglet.app.run()
+        """ Instantiating the robot's egocentric spatial memory window """
+        self.osoyoo_car_window = EgoMemoryWindow(600)
+        self.osoyoo_car_window.zoom_level = 2
+
+        self.osoyoo_car_window.dispatch_events()
+        self.osoyoo_car_window.on_draw()
+        self.osoyoo_car_window.flip()
 
     def outcome(self, action):
         """ Enacting an action and returning the outcome """
         outcome = 0
         if action == 0:
-            outcome = self.em_window.outcome('8')
+            outcome = self.osoyoo_car_window.on_text('8')
         if action == 1:
-            outcome = self.em_window.outcome('1')
+            outcome = self.osoyoo_car_window.on_text('1')
         if action == 2:
-            outcome = self.em_window.outcome('3')
+            outcome = self.osoyoo_car_window.on_text('3')
+
+        # Inspired by https://stackoverflow.com/questions/61217265/
+        pyglet.clock.tick()
+        self.osoyoo_car_window.dispatch_events()
+        self.osoyoo_car_window.on_draw()
+        self.osoyoo_car_window.flip()
+
         return int(outcome)
 
 
 if __name__ == "__main__":
+    """ Run in interactive mode """
     e = OsoyooCarEnacter()
+    pyglet.app.run()
