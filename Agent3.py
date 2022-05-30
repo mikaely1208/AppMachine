421a01acd0c16a3f63651793bbd76627906ab37b
andrianandraina.mika <mikaely.andrianandraina@esqese.ucly.fr>
2022-05-30 21:55:25 +0200

    finalisation agent 3 test.7

--- Agent3.py
+++ Agent3.py
@@ -2,7 +2,7 @@
 import random
 
 
-class Agent2:
+class Agent3:
 
     def __init__(self, _hedonist_table):
         """ Creating our agent """
@@ -11,9 +11,7 @@ class Agent2:
         self.previous_action = 0
         self.anticipated_outcome = None
         self.counter = 0
-        self.previous_outcome = 0
-        self.outcome_for_action1 = 0
-        self.outcome_for_action0 = 0
+        self.hedonist_table = [[-1, 1], [-1, 1]]
 
     def action(self, outcome):
         """ tracing the previous cycle """
@@ -25,19 +23,7 @@ class Agent2:
                       ", valence: " + str(self.hedonist_table[self._action][outcome]) +
                       "; counter: " + str(self.counter) + ")")
 
-
-        # Choisir la prochaine action
-        if self._action == 0:
-            self.outcome_for_action0 = outcome
-            self._action = 1
+        if self.hedonist_table[self._action][outcome] == 1:
+            return self._action
         else:
-            self.outcome_for_action1 = outcome
-            self._action = 0
-
-        # annonce l'anticipation
-        if self._action == 0:
-            self.anticipated_outcome = self.outcome_for_action0
-        else:
-            self.anticipated_outcome = self.outcome_for_action1
-
-        return self._action
+            return 0
--- world.py
+++ world.py
@@ -1,59 +1,53 @@
-#!/usr/bin/env python
-# Olivier Georgeon, 2021.
-# This code is used to teach Developmental AI.
+# !/usr/bin/env python
 # from turtlesim_enacter import TurtleSimEnacter # requires ROS
 from turtlepy_enacter import TurtlePyEnacter
-# from Agent5 import Agent5
-# from OsoyooCarEnacter import OsoyooCarEnacter
 import random
 from Agent2 import Agent2
+from Agent3 import Agent3
+from Agent5 import Agent5
+from OsoyooCarEnacter import OsoyooCarEnacter
+ROBOT_IP = "192.168.4.1"
 
 
 class Agent:
-    def __init__(self, _hedonist_table):
+    def __init__(self, valence_table):
         """ Creating our agent """
-        self.hedonist_table = _hedonist_table
-        self._action = None
+        self.valence_table = valence_table
+        self._action = 0
         self.anticipated_outcome = None
-        self.compteur = 0
-        self.previous_outcome = None
+        self.counter = 0
+        self.previous_outcome = 0
+
     def action(self, outcome):
         """ tracing the previous cycle """
-        if self.previous_outcome == outcome:
-            self.compteur+=1
-        else:
-            self.compteur = 0
-
         if self._action is not None:
-            print("Action: " + str(self._action) +
-                  ", Anticipation: " + str(self.anticipated_outcome) +
-                  ", Outcome: " + str(outcome) +
-                  ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
-                  ", valence: " + str(self.hedonist_table[self._action][outcome]) +
-                  ", compteur: " + str(self.compteur) + ")")
-        #self.compteur
+                print("Action: " + str(self._action) +
+                      ", Anticipation: " + str(self.anticipated_outcome) +
+                      ", Outcome: " + str(outcome) +
+                      ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+                      ", valence: " + str(self.valence_table[self._action][outcome]) +
+                      "; counter: " + str(self.counter) + ")")
 
         """ Computing the next action to enact """
-
         # TODO: Implement the agent's decision mechanism
-        self._action = 0
+        # if outcome == self.previous_outcome:
+        #     self.counter += 1
+        # if self.counter == 4:
+        #     if self._action == 0:
+        #         self._action = 1
+        #     else:
+        #         self._action = 0
+        #     self.counter = 0
+        # self.previous_outcome = outcome
 
-        if self.compteur >= 2:
-                #self._action = 1
-            self.compteur = 0
-            if self._action == 0:
-                self._action = 1
-
-            elif self._action == 1:
-                self._action = 0
 
 
-        # TODO: Implement the agent's anticipation mechanism
+
+
+        #     self.previous_outcome = outcome
+        # # TODO: Implement the agent's anticipation mechanism
         self.anticipated_outcome = 0
-        self.previous_outcome = outcome
         return self._action
-
-
 
 
 class Environment1:
@@ -68,6 +62,7 @@ class Environment1:
 
 class Environment2:
     """ In Environment 2, action 0 yields outcome 1, action 1 yields outcome 0 """
+
     def outcome(self, action):
         if action == 0:
             return 1
@@ -77,6 +72,7 @@ class Environment2:
 
 class Environment3:
     """ Environment 3 yields outcome 1 only when the agent alternates actions 0 and 1 """
+
     def __init__(self):
         """ Initializing Environment3 """
         self.previous_action = 0
@@ -88,29 +84,30 @@ class Environment3:
         self.previous_action = action
         return _outcome
 
+
+
 class Environment4:
     def outcome(self, action):
         return random.randint(0, 1)
 
 
 # TODO Define the hedonist valance of interactions (action, outcome)
-hedonist_table = [[-1, 1], [-1, 1]]
+valence_table = [[-1, 1], [-1, 1]]
 # TODO Choose an agent
-a = Agent(hedonist_table)
-# a = Agent5(hedonist_table)
+# a = Agent2(hedonist_table)
+a = Agent3(valence_table)
 # TODO Choose an environment
-#e = Environment1()
-#e = Environment2()
+e = Environment1()
+# e = Environment2()
 # e = Environment3()
-e = Environment4()
+# e = Environment4()
 # e = TurtleSimEnacter()
 # e = TurtlePyEnacter()
-# e = OsoyooCarEnacter()
+# e = OsoyooCarEnacter(ROBOT_IP)
 
 if __name__ == '__main__':
-    """ The main loop controlling the interaction of the agent with uwu the environment """
+    """ The main loop controlling the interaction of the agent with the environment """
     outcome = 0
-    for i in range(70):
+    for i in range(10):
         action = a.action(outcome)
         outcome = e.outcome(action)
-
