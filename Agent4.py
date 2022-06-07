e473d1f450a261b09b4ef15aa9c177c61c077e91
andrianandraina.mika <mikaely.andrianandraina@esqese.ucly.fr>
2022-06-07 10:11:04 +0200

    ajout du readme dans doc

--- Agent4.py
+++ Agent4.py
@@ -1,41 +1,43 @@
-# !/usr/bin/env python
 import random
-
-
 class Agent4:
-
     def __init__(self, valence_table):
         """ Creating our agent """
         self.outcome_for_action2 = 0
         self.valence_table = valence_table
         self._action = 0
-        self.previous_action = 0
         self.anticipated_outcome = None
         self.counter = 0
-        self.previous_outcome = 0
-        self.outcome_for_action1 = 0
+        self.previous_outcome = None
+        self.previous_action = 0
         self.outcome_for_action0 = 0
+        self.outcome_for_action1 = 0
 
     def action(self, outcome):
         """ tracing the previous cycle """
-        if self._action is not None:
-                print("Action: " + str(self._action) +
-                      ", Anticipation: " + str(self.anticipated_outcome) +
-                      ", Outcome: " + str(outcome) +
-                      ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
-                      ", valence: " + str(self.valence_table[self._action][outcome]) +
-                      "; counter: " + str(self.counter) + ")")
 
-            # Choisir la prochaine action
-        self._action = random.randint(0, 2)
+        if self._action is not None:
+            print("Action: " + str(self._action) +
+                  ", Anticipation: " + str(self.anticipated_outcome) +
+                  ", Outcome: " + str(outcome) +
+                  ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+                  ", valence: " + str(self.valence_table[self._action][outcome]) +
+                  "; counter: " + str(self.counter) + ")")
+
+        #self._action = random.randint(0, 2)
         if self._action == 0:
             self.outcome_for_action0 = outcome
+        if self.outcome_for_action0 == 1:
             self._action = 1
-        elif self._action == 1:
             self.outcome_for_action1 = outcome
-            self._action = 2
-        else:
-            self.outcome_for_action2 = outcome
+        if self.outcome_for_action1 == 0:
             self._action = 0
+            self.outcome_for_action0 = outcome
+        #     self._action = 1
+        # elif self._action == 1:
+        #     self.outcome_for_action1 = outcome
+        #     self._action = 2
+        # else:
+        #     self.outcome_for_action2 = outcome
+        #     self._action = 0
 
         return self._action
--- world.py
+++ world.py
@@ -1,53 +1,76 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
+# Olivier Georgeon, 2021.
+# This code is used to teach Developmental AI.
 # from turtlesim_enacter import TurtleSimEnacter # requires ROS
 from turtlepy_enacter import TurtlePyEnacter
-import random
-from Agent2 import Agent2
 from Agent3 import Agent3
+#from Agent2 import Agent2
+import random
 from Agent4 import Agent4
-# from OsoyooCarEnacter import OsoyooCarEnacter
+#from Agent5 import Agent5
+#from OsoyooCarEnacter import OsoyooCarEnacter
 ROBOT_IP = "192.168.4.1"
+
 
 
 class Agent:
     def __init__(self, valence_table):
         """ Creating our agent """
         self.valence_table = valence_table
-        self._action = 0
-        self.anticipated_outcome = None
+        self._action = None
+        self.anticipated_outcome = 0
         self.counter = 0
-        self.previous_outcome = 0
-
+        self.previous_outcome = None
     def action(self, outcome):
         """ tracing the previous cycle """
+        if self.previous_outcome == outcome:
+            self.counter += 1
+        else:
+            self.counter = 0
+
         if self._action is not None:
-                print("Action: " + str(self._action) +
-                      ", Anticipation: " + str(self.anticipated_outcome) +
-                      ", Outcome: " + str(outcome) +
-                      ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
-                      ", valence: " + str(self.valence_table[self._action][outcome]) +
-                      "; counter: " + str(self.counter) + ")")
+            print("Action: " + str(self._action) +
+                  ", Anticipation: " + str(self.anticipated_outcome) +
+                  ", Outcome: " + str(outcome) +
+                  ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+
+                  ", valence: " + str(self.valence_table[self._action][outcome]) +
+                  "; counter: " + str(self.counter) + ")")
+            #self.counter+=1
 
         """ Computing the next action to enact """
         # TODO: Implement the agent's decision mechanism
-        # if outcome == self.previous_outcome:
-        #     self.counter += 1
-        # if self.counter == 4:
-        #     if self._action == 0:
+        #self._action = 0
+
+        # while (x<6):
+        #     self._action = 0
+        #
+        #     x+=1
+        # else:
+        #     self._action = 1
+        # if self._action == 0:
         #         self._action = 1
-        #     else:
-        #         self._action = 0
+
+        # if self.counter >= 2:
+        #     #self._action = 1
         #     self.counter = 0
-        # self.previous_outcome = outcome
+        #     if self._action == 1:
+        #         self._action = 0
+        #     elif self._action == 0:
+        #         self._action = 1
+
+        #     if self.counter > 10:
+        #         self._action = 0
+        #         self.counter = 0
 
 
-
-
-
-        #     self.previous_outcome = outcome
-        # # TODO: Implement the agent's anticipation mechanism
+        # TODO: Implement the agent's anticipation mechanism
         self.anticipated_outcome = 0
+        #self.previous_outcome = outcome
         return self._action
+
+        #if counter == 4:
+            #outcome =
 
 
 class Environment1:
@@ -62,7 +85,6 @@ class Environment1:
 
 class Environment2:
     """ In Environment 2, action 0 yields outcome 1, action 1 yields outcome 0 """
-
     def outcome(self, action):
         if action == 0:
             return 1
@@ -72,7 +94,6 @@ class Environment2:
 
 class Environment3:
     """ Environment 3 yields outcome 1 only when the agent alternates actions 0 and 1 """
-
     def __init__(self):
         """ Initializing Environment3 """
         self.previous_action = 0
@@ -84,19 +105,22 @@ class Environment3:
         self.previous_action = action
         return _outcome
 
-
-
 class Environment4:
     def outcome(self, action):
         return random.randint(0, 1)
 
 
-# TODO Define the hedonist valance of interactions (action, outcome)
-valence_table = [[-1, 1], [-1, 1], [0, 2]]
+# TODO Define the valance of interactions (action, outcome)
+valences = [[-1, 1], [-1, 1],[0, 2]]
+# valences = [[1, -1], [1, -1]]
 # TODO Choose an agent
-# a = Agent2(hedonist_table)
-# a = Agent3(valence_table)
-a = Agent4(valence_table)
+
+# a = Agent2(valences)
+# a = Agent3(valences)
+# a = Agent(valences)
+# a = Agent5(valences)
+a = Agent4(valences)
+
 # TODO Choose an environment
 # e = Environment1()
 # e = Environment2()
@@ -109,6 +133,6 @@ e = TurtlePyEnacter()
 if __name__ == '__main__':
     """ The main loop controlling the interaction of the agent with the environment """
     outcome = 0
-    for i in range(10):
+    for i in range(40):
         action = a.action(outcome)
         outcome = e.outcome(action)
